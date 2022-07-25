2ffe1190e766a54e74ceedaf71e7ba88d2a59538
Tristan <tribot17@gmail.com>
2022-07-25 16:55:56 +0200

    update

--- test/mainContractTest.js
+++ test/mainContractTest.js
@@ -20,15 +20,75 @@ contract("mainContract", (accounts) => {
     this.mainConctractInstance = await mainConctract.new(5, { from: owner });
   });
 
+  /*Test : 
+    Ajouter un mot
+    Lancer la session
+    Tirer les mots au hasard
+    Deviner une bonne lettre
+    Deviner une mauvaise lettre
+    Le score doit augmenter
+    Si la lettre est plusieurs dans le mot
+    Une fois tous les mots trouvés toutes les infos sont sensé se reset
+    Withdraw function
+  */
   context("------- Test creation session -------", () => {
-    it(`${testCounter++} : Devrait créer un session`, async function () {
+    beforeEach(async function () {
       await this.mainConctractInstance.createSession("1", {
         from: player1,
         value: "1",
       });
-      const session = await this.mainConctractInstance.session(1);
+      this.session = await this.mainConctractInstance.session(1);
+    });
 
-      await expect(session.player1).to.be.equal(player1);
+    it(`${testCounter++} : L'id de la session devrait être égal à 1`, async function () {
+      await expect(this.session.sessionId).to.be.bignumber.equal("1");
+    });
+
+    it(`${testCounter++} : Le joueur 1 devrait être le createur de la session`, async function () {
+      await expect(this.session.player1).to.be.bignumber.equal(player1);
+    });
+
+    it(`${testCounter++} : La valeur de pari devrait être égal à 1`, async function () {
+      await expect(this.session.bet).to.be.bignumber.equal("1");
+    });
+
+    it(`${testCounter++} : Le joueur 2 devrait être null`, async function () {
+      await expect(this.session.player2).to.be.bignumber.equal(
+        "0x0000000000000000000000000000000000000000"
+      );
+    });
+
+    it(`${testCounter++} : La session ne devrait pas être complète`, async function () {
+      await expect(this.session.isComplete).to.be.equal(false);
+    });
+
+    it(`${testCounter++} : La partie ne devrait pas être commencé`, async function () {
+      await expect(this.session.started).to.be.equal(false);
+    });
+
+    it(`${testCounter++} : La partie ne devrait pas être terminé`, async function () {
+      await expect(this.session.ended).to.be.equal(false);
+    });
+  });
+
+  context("------- Test rejoindre une session -------", () => {
+    beforeEach(async function () {
+      await this.mainConctractInstance.createSession("1", {
+        from: player1,
+        value: "1",
+      });
+      this.session = await this.mainConctractInstance.session(1);
+    });
+
+    it(`${testCounter++} : Le joueur 2 devrait rejoindre la session 1`, async function () {
+      const balanceBefore = player2.balance();
+      await this.mainConctractInstance.joinSession("1", {
+        from: player2,
+        value: "1",
+      });
+      const balanceAfter = player2.balance();
+      await expect(balanceAfter).to.be.bignumber.equal(balanceBefore - 1);
+      await expect(this.session.player2).to.bignumber.be.equal(player2);
     });
   });
 });
